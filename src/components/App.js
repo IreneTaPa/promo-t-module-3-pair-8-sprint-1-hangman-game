4dcbdff6cbf8375983ef56ed094ecf0689343cbc
IreneTaPa <italavero@hotmail.com>
2023-06-19 11:21:24 +0200

    add initial html and sccs

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,72 @@
-// Fichero src/components/App.js
-import '../styles/App.scss';
-//imports: react, archivos propios, Sass, images
+import '../styles/main.scss';
+import '../images/blackboard.jpg';
+import '../fonts/KgTenThousandReasons-R1ll.ttf';
 
-/*Componente */
 function App() {
-  /*variables de estado, funciones manejadoras de evento, variables normales, funciones handle*/
   return (
-    /*return -> HTML */
-    <div>
-      <h1 className="h1">Hola mundo</h1>
+    <div className="page">
+      <header>
+        <h1 classNameName="header__title">Juego del ahorcado</h1>
+      </header>
+      <main className="main">
+        <section>
+          <div className="solution">
+            <h2 className="title">Solución:</h2>
+            <ul className="letters">
+              <li className="letter">k</li>
+              <li className="letter">a</li>
+              <li className="letter"></li>
+              <li className="letter">a</li>
+              <li className="letter">k</li>
+              <li className="letter">r</li>
+              <li className="letter"></li>
+              <li className="letter">k</li>
+              <li className="letter">e</li>
+              <li className="letter">r</li>
+            </ul>
+          </div>
+          <div className="error">
+            <h2 className="title">Letras falladas:</h2>
+            <ul className="letters">
+              <li className="letter">f</li>
+              <li className="letter">q</li>
+              <li className="letter">h</li>
+              <li className="letter">p</li>
+              <li className="letter">x</li>
+            </ul>
+          </div>
+          <form className="form">
+            <label className="title" for="last-letter">
+              Escribe una letra:
+            </label>
+            <input
+              autocomplete="off"
+              className="form__input"
+              maxlength="1"
+              type="text"
+              name="last-letter"
+              id="last-letter"
+            />
+          </form>
+        </section>
+        <section className="dummy error-5">
+          <span className="error-13 eye"></span>
+          <span className="error-12 eye"></span>
+          <span className="error-11 line"></span>
+          <span className="error-10 line"></span>
+          <span className="error-9 line"></span>
+          <span className="error-8 line"></span>
+          <span className="error-7 line"></span>
+          <span className="error-6 head"></span>
+          <span className="error-5 line"></span>
+          <span className="error-4 line"></span>
+          <span className="error-3 line"></span>
+          <span className="error-2 line"></span>
+          <span className="error-1 line"></span>
+        </section>
+      </main>
     </div>
   );
 }
-/*EXPORT */
+
 export default App;
