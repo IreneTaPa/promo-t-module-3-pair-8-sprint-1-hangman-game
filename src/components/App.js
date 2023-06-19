47fdbbcd0bfed39b64fc7cc123ea8951883b17d7
IreneTaPa <italavero@hotmail.com>
2023-06-19 10:46:39 +0200

    create repo

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// Fichero src/components/App.js
+import '../styles/App.scss';
+//imports: react, archivos propios, Sass, images
+
+/*Componente */
+function App() {
+  /*variables de estado, funciones manejadoras de evento, variables normales, funciones handle*/
+  return (
+    /*return -> HTML */
+    <div>
+      <h1 className="h1">Hola mundo</h1>
+    </div>
+  );
+}
+/*EXPORT */
+export default App;
