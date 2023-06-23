be7e2c2ca2947504acdfeeaf6b372b712d72ac19
Lourdesjupo <lourdes.jupo@gmail.com>
2023-06-23 11:28:56 +0200

    3.4 exercise 4

--- src/components/App.js
+++ src/components/App.js
@@ -8,12 +8,14 @@ function App() {
   const [lastLetter, setLastLetter] = useState('');
   const [word, setWord] = useState('pepino');
   const [userLetters, setUserLetter] = useState([]);
+  let count = 0
   console.log(userLetters);
+
   const renderSolutionLetters = () => {
     const wordLetters = word.split('');
     return wordLetters.map((word, idx) => {
       return (
-        <li key={idx} className="letter">
+        <li key={idx} className='letter'>
           {userLetters.map((letter) => {
             return letter === word ? letter : '';
           })}
@@ -22,15 +24,31 @@ function App() {
     });
   };
   const renderErrorLetters = () => {
-    userLetters.filter((letter) => {
-      return letter !== word;
-    }).map;
+    // debugger;
+    const wordLetters = word.split('');
+    return userLetters
+      .filter((letter) => {
+        const x = wordLetters.indexOf(letter);
+        return x < 0 ? letter : '' 
+      })
+      .map((wrongWord, idx) => {
+        count++
+        return (
+          <li className='letter' key={idx}>
+            {wrongWord}
+            {handleClick()}
+          </li>
+        );
+        
+      });
   };
+
   const handleClick = () => {
     if (numberOfErrors > 13) {
       setErrors(0);
+      count = 0
     } else {
-      setErrors(numberOfErrors + 1);
+      setErrors(count);
     }
   };
   const handleLastLetter = (ev) => {
@@ -48,16 +66,16 @@ function App() {
   };
 
   return (
-    <div className="page">
+    <div className='page'>
       <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
+        <h1 className='header__title'>Juego del ahorcado</h1>
       </header>
-      <main className="main">
+      <main className='main'>
         <section>
-          <div className="solution">
-            <h2 className="title">Solución:</h2>
+          <div className='solution'>
+            <h2 className='title'>Solución:</h2>
 
-            <ul className="letters">
+            <ul className='letters'>
               {renderSolutionLetters()}
               {/* <li className='letter'>k</li>
               <li className='letter'>a</li>
@@ -71,10 +89,10 @@ function App() {
               <li className='letter'>r</li> */}
             </ul>
           </div>
-          <div className="error">
-            <h2 className="title">Letras falladas:</h2>
-            <ul className="letters">
-              {renderErrorLetters}
+          <div className='error'>
+            <h2 className='title'>Letras falladas:</h2>
+            <ul className='letters'>
+              {renderErrorLetters()}
               {/*<li className="letter">f</li>
               <li className="letter">q</li>
               <li className="letter">h</li>
@@ -82,17 +100,17 @@ function App() {
             <li className="letter">x</li>*/}
             </ul>
           </div>
-          <form className="form">
-            <label className="title" htmlFor="last-letter">
+          <form className='form'>
+            <label className='title' htmlFor='last-letter'>
               Escribe una letra:
             </label>
             <input
-              autoComplete="off"
-              className="form__input"
-              maxLength="1"
-              type="text"
-              name="last-letter"
-              id="last-letter"
+              autoComplete='off'
+              className='form__input'
+              maxLength='1'
+              type='text'
+              name='last-letter'
+              id='last-letter'
               onChange={handleLastLetter}
               value={lastLetter}
             />
@@ -100,19 +118,19 @@ function App() {
           <button onClick={handleClick}>Incrementar</button>
         </section>
         <section className={`dummy error-${numberOfErrors}`}>
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
+          <span className='error-13 eye'></span>
+          <span className='error-12 eye'></span>
+          <span className='error-11 line'></span>
+          <span className='error-10 line'></span>
+          <span className='error-9 line'></span>
+          <span className='error-8 line'></span>
+          <span className='error-7 line'></span>
+          <span className='error-6 head'></span>
+          <span className='error-5 line'></span>
+          <span className='error-4 line'></span>
+          <span className='error-3 line'></span>
+          <span className='error-2 line'></span>
+          <span className='error-1 line'></span>
         </section>
       </main>
     </div>
